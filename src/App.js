787cb2c6604d2749cc4bbbce0237c791485cf511
Ben Abbott <babbott85@gmail.com>
2022-04-16 10:44:48 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Fav from "./assets/favicon.ico"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1 class="row justify-content-start"><img src={Fav} alt="logo"/> Ben Abbott</h1>
+      
     </div>
   );
 }
