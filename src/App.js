2465ea5b8a90a12a2e4ae5f65e5f9413fac7eb21
Ben Abbott <babbott85@gmail.com>
2022-07-12 11:28:08 -0600

    background

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Fav from "./assets/favicon.ico"
 import Projects from './components/projects'
+import Card from "./components/muicard"
+
+import Carousel from './components/carousel'
 function App() {
   return (
     <div className="">
+      
       <h1 class="row justify-content-start"><img src={Fav} alt="logo"/> Ben Abbott</h1>
-      <Projects/>
+      {/* <Projects/> */}
+      <Card/>
+     
     </div>
   );
 }
