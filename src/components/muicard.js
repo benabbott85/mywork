9f6501ad3c7ab9232283a77fd2debe400c667cad
Ben Abbott <babbott85@gmail.com>
2022-07-12 13:50:40 -0600

    css

--- src/components/muicard.js
+++ src/components/muicard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {Container, Row, Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from "reactstrap"
+import Max from "../assets/maxps.jpg"
 function testCards(){
   return(
     <>
     <Container>
   <Row>
     <Col lg={3}>
-      <Card>
-        <CardTitle>Test</CardTitle>
-        <CardBody>lorem ipsum</CardBody>
+      <Card className="maxfund">
+        <CardTitle className="maxlink">MaxFund Animal Adoption Center </CardTitle>
+        <CardImg className="max"src={Max}></CardImg>
+        <h5 className="maxlink">www.maxfund.org</h5>
       </Card>
     </Col>
     <Col className="testing"lg={3}>
