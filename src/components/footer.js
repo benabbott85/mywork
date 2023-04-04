5c63953314cde15dfc49f99de64eef57d30b7c7f
Ben Abbott <babbott85@gmail.com>
2023-04-04 14:41:32 -0600

    deploying

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer (){
         <>
         <Row>
         <div class="footalign">
-        <a className="foot" href="" target="_blank" rel="noreferrer"><h3><strong>Resume</strong></h3></a>
+        <a className="foot"  target="_blank" rel="noreferrer"><h3><strong>Resume</strong></h3></a>
         <a href="mailto:babbott85@gmail.com" target="_blank" rel="noreferrer"><i class="fas fa-envelope fa-4x " target='_blank'></i></a>
         <a className="linkedin"href="https://www.linkedin.com/in/ben-abbott-51394421/" target="_blank" rel="noreferrer" ><i class="fab fa-linkedin fa-4x linkedin" target='_blank'></i></a>
         </div>
