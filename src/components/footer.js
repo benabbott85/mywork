8606619cd48892da686536b27b0015d90c7ee40d
Ben Abbott <babbott85@gmail.com>
2023-04-05 09:51:04 -0600

    more edits

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ function Footer (){
         <>
         <Row>
         <div class="footalign">
-        <a src={Resume} target="_blank"><h3><strong>Resume</strong></h3></a>
+        <a href={Resume} target="_blank"><h3><strong>Resume</strong></h3></a>
         <a href="mailto:babbott85@gmail.com" target="_blank" rel="noreferrer"><i class="fas fa-envelope fa-4x " target='_blank'></i></a>
         <a className="linkedin"href="https://www.linkedin.com/in/ben-abbott-51394421/" target="_blank" rel="noreferrer" ><i class="fab fa-linkedin fa-4x linkedin" target='_blank'></i></a>
         </div>
