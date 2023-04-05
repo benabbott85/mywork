34af75c86f98b0c538bac0c07bc5bda5063c9ca9
Ben Abbott <babbott85@gmail.com>
2023-04-05 09:26:34 -0600

    resume added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React from "react"
 import {Row} from "reactstrap"
+import Resume from '../assets/Ben Abbott.pdf'
 function Footer (){
     return(
         <>
         <Row>
         <div class="footalign">
-        <h3><strong>Resume</strong></h3>
+        <a href={Resume} target="_blank"><h3><strong>Resume</strong></h3></a>
         <a href="mailto:babbott85@gmail.com" target="_blank" rel="noreferrer"><i class="fas fa-envelope fa-4x " target='_blank'></i></a>
         <a className="linkedin"href="https://www.linkedin.com/in/ben-abbott-51394421/" target="_blank" rel="noreferrer" ><i class="fab fa-linkedin fa-4x linkedin" target='_blank'></i></a>
         </div>
